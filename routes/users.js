0dd0806658166a1e940a1e1647b26b4085f47cb4
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-02-27 12:02:27 +0300

    register user

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-router.get('/register', (req, res, next) => {
-  res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+  let body = _.pick(req.body, ['name', 'email', 'username', 'password']),
+      newUser = new User(body);
+
+
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({ success: false, msg: 'Failed to register user' });
+    } else {
+      res.json({ success: true, msg: 'User registered' });
+    }
+  });
 });
 
-router.get('/authenticate', (req, res, next) => {
+
+router.post('/authenticate', (req, res, next) => {
   res.send('Authenticate');
 });
 
